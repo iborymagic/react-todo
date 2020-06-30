23c53a4df71393dd1359ce60fd60c1ee3d899a99
iborymagic <mlch1603@naver.com>
2020-06-30 17:13:59 +0900

    Updates

---
+++ precache-manifest.550ac846b199c82487f5f5cb4fb0ad28.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927236c516364984422942e9aed8009c",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "663119f3f5e7fc46b382",
+    "url": "/react-todo/static/css/main.51115ce3.chunk.css"
+  },
+  {
+    "revision": "9d06cc764a8a5c6c430e",
+    "url": "/react-todo/static/js/2.3c7fac71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.3c7fac71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663119f3f5e7fc46b382",
+    "url": "/react-todo/static/js/main.fb298695.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  },
+  {
+    "revision": "7a9f18fff976271b3520c4b088b2f1cb",
+    "url": "/react-todo/static/media/1.7a9f18ff.jpg"
+  },
+  {
+    "revision": "379ce94d2ccd809414e71226a791a060",
+    "url": "/react-todo/static/media/check.379ce94d.svg"
+  },
+  {
+    "revision": "49a6f111ffa7009cf67031d8393e24d8",
+    "url": "/react-todo/static/media/delete.49a6f111.svg"
+  },
+  {
+    "revision": "2cf13a7567958346a40325b0a7805059",
+    "url": "/react-todo/static/media/dots.2cf13a75.svg"
+  },
+  {
+    "revision": "66c01ec98ea86b18abdd47baad2cb17e",
+    "url": "/react-todo/static/media/eraser.66c01ec9.svg"
+  },
+  {
+    "revision": "79e55f938753c809777ea2044e735ec5",
+    "url": "/react-todo/static/media/list.79e55f93.svg"
+  },
+  {
+    "revision": "ea758e8cf2a447f16d57cc67379e7ac0",
+    "url": "/react-todo/static/media/memo.ea758e8c.svg"
+  },
+  {
+    "revision": "92b54d76a2ae3ac6964bde1335a6193c",
+    "url": "/react-todo/static/media/pin.92b54d76.svg"
+  },
+  {
+    "revision": "fee0290b8d825119c957457adb76b1df",
+    "url": "/react-todo/static/media/pin_menu.fee0290b.svg"
+  },
+  {
+    "revision": "03152b7fa9826977deec6b4d75922686",
+    "url": "/react-todo/static/media/pin_out.03152b7f.svg"
+  }
+]);

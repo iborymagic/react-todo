2dbf3213ae90ec47eeaa40bd525b3b42aaf647e2
iborymagic <mlch1603@naver.com>
2020-06-30 17:09:33 +0900

    added pin system, lots of bugs fixed

--- src/App.js
+++ src/App.js
@@ -21,9 +21,31 @@ class App extends Component {
 
     render() {
         const itemArr = [];
+        const pinnedTodos = this.state.todos.filter(el => el.pinned);
+        
+        let lastPinId = -10;
+        if(pinnedTodos.length > 0) {
+            lastPinId = pinnedTodos[pinnedTodos.length - 1].id;
+        }
         this.state.todos.map(cur => {
             if(cur.date === this.state.today) {
-                itemArr.push(<ListItem key={cur.id} todo={cur} onCheckChanged={function(item_id) {
+                itemArr.push(<ListItem key={cur.id} todo={cur} last_id={lastPinId} onCheckChanged={function(item_id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === item_id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr[i].done = !_arr[i].done;
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
                     this.state.todos.forEach((el, idx) => {
                         if(el.id === item_id) {
                             const _arr = Array.from(this.state.todos);
@@ -34,9 +56,25 @@ class App extends Component {
 
                             this.setState({ todos : _arr });
                         }
-                    });            
+                    });*/
                 }.bind(this)}
                 onDelete={function(item_id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === item_id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr.splice(i, 1);
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
                     this.state.todos.forEach((el, idx) => {
                         if(el.id === item_id) {
                             const _arr = Array.from(this.state.todos);
@@ -47,9 +85,25 @@ class App extends Component {
 
                             this.setState({ todos : _arr });
                         }
-                    });
+                    });*/
                 }.bind(this)}
                 onMemoInput={function(_memo, _id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr[i].memo = _memo;
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
                     this.state.todos.forEach((el, idx) => {
                         if(_id === el.id) {
                             const _arr = Array.from(this.state.todos);
@@ -60,9 +114,25 @@ class App extends Component {
 
                             this.setState({ todos : _arr });
                         }
-                    });
+                    });*/
                 }.bind(this)}
                 onMemoDelete={function(_id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr[i].memo = "";
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
                     this.state.todos.forEach((el, idx) => {
                         if(el.id === _id) {
                             const _arr = Array.from(this.state.todos);
@@ -73,13 +143,91 @@ class App extends Component {
 
                             this.setState({ todos : _arr });
                         }
-                    });
+                    });*/
+                }.bind(this)}
+                onPin={function(_id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            const tmp = _arr[i];
+                            _arr.splice(i, 1);
+                            tmp.pinned = true;
+                            _arr.unshift(tmp);
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
+                    this.state.todos.forEach((el, idx) => {
+                        if(el.id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr.splice(idx, 1);
+                            el.pinned = true;
+                            _arr.unshift(el);
+
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                        }
+                    });*/
+                }.bind(this)}
+                onPinOut={function(_id) {
+                    let i = 0;
+                    const len = this.state.todos.length;
+                    
+                    for(i = 0; i < len; i++) {
+                        if(this.state.todos[i].id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            const tmp = _arr[i];
+                            _arr.splice(i, 1);
+                            tmp.pinned = false;
+                            _arr.push(tmp);
+                            
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                            break;
+                        }
+                    }
+                    /*
+                    this.state.todos.forEach((el, idx) => {
+                        if(el.id === _id) {
+                            const _arr = Array.from(this.state.todos);
+                            _arr.splice(idx, 1);
+                            el.pinned = false;
+                            _arr.push(el);
+                            
+                            localStorage.removeItem('todos');
+                            localStorage.setItem('todos', JSON.stringify(_arr));
+
+                            this.setState({ todos : _arr });
+                        }
+                    });*/
                 }.bind(this)}></ListItem>);
             }
         });
 
         return (
-            <div className="App">
+            <div className="App" onClick={function(e) {
+                if(!e.target.classList.contains('item-menu')) {
+                    const openTooltips = document.getElementsByClassName('item-tooltip-open');
+                    if(openTooltips) {
+                        Array.from(openTooltips).forEach(el => {      
+                            el.classList.toggle('item-tooltip-open');
+                            el.classList.toggle('item-tooltip-close');
+                        });
+                    }
+                }
+            }}>
                 <div className="background"></div>
                 <div className="frame">
                     <div className="title-div">
@@ -94,7 +242,8 @@ class App extends Component {
                         } else {
                             let arr = Array.from(this.state.todos);
                             arr.push({
-                                id : this.state.todos.length > 0 ? this.state.todos[this.state.todos.length - 1].id + 1 : 0,
+                                id : Math.random().toString(36).substr(2, 9),
+                                /*this.state.todos.length > 0 ? this.state.todos[this.state.todos.length - 1].id + 1 : 0,*/
                                 text : _text,
                                 memo : "",
                                 pinned : false,
@@ -108,6 +257,7 @@ class App extends Component {
                     }.bind(this)}></AddTask>
                     <div className="list-div">
                         {itemArr}
+                        <div className="blank"></div>
                     </div>
                 </div>
             </div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -12,7 +12,7 @@ class ListItem extends Component {
     render() {
         const item_id = this.state.todo.id;
         return(
-            <div className="item">
+            <div className={this.state.todo.memo ? `item-${item_id}` : (this.props.last_id === item_id ? `item-${item_id} pin-bottom` : `item-${item_id}`)}>
                 <div className="item-div">
                     {/*<input type="checkbox" className="item-check" id={`item-check-${item_id}`}></input>*/}
                     {this.state.todo.done ? 
@@ -24,16 +24,46 @@ class ListItem extends Component {
                     }.bind(this)}></input>}
                     <label htmlFor={`item-check-${item_id}`} className="item-label"></label>
                     <div className="item-todo">{this.state.todo.text}</div>
-                    <div className="item-pin"></div>
-                    <div className="item-menu" id={`item-menu-${item_id}`} onClick={function() {
+                    <div className={this.state.todo.pinned ? "item-pin" : "item-pin pin-icon-invisible"} id={`item-pin-${item_id}`}></div>
+                    <div className="item-menu" id={`item-menu-${item_id}`} onClick={function(e) {
+                        /* close every menu different from menu that i clicked this time(among open ones). */
+                        const others = document.getElementsByClassName('item-tooltip-open');
+                        if(others) {
+                            Array.from(others).forEach(el => {
+                                if(e.target.nextSibling.childNodes[0].id !== el.id) {
+                                    el.classList.remove('item-tooltip-open');
+                                    el.classList.add('item-tooltip-close');
+                                }
+                            });
+                        }
                         document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
                         document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');
                     }}></div>
                     <div className="item-tooltip">
                         <ul className="item-tooltip-ul item-tooltip-close" id={`item-tooltip-${item_id}`}>
-                            <li className="item-tooltip-li">
+                            <li className={!this.state.todo.pinned ? 'item-tooltip-li' : 'item-tooltip-li pin-unable'}>
                                 <div className="item-pin-icon"></div>
-                                <div className="item-pin-text">Pin on the top</div>
+                                <div className="item-pin-text" onClick={function() {
+                                    document.getElementById(`item-pin-${item_id}`).classList.toggle('pin-icon-invisible');
+
+                                    const _todo = Object.assign({}, this.state.todo);
+                                    _todo.pinned = true;
+                                    this.setState({ todo : _todo });
+                                    this.props.onPin(item_id);
+                                    /*document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
+                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');*/
+                                }.bind(this)}>Pin on the top</div>
+                            </li>
+                            <li className={this.state.todo.pinned ? 'item-tooltip-li' : 'item-tooltip-li pin-unable'}>
+                                <div className="item-pin-out-icon"></div>
+                                <div className="item-pin-out-text" onClick={function() {
+                                    document.getElementById(`item-pin-${item_id}`).classList.toggle('pin-icon-invisible');
+
+                                    const _todo = Object.assign({}, this.state.todo);
+                                    _todo.pinned = false;
+                                    this.setState({ todo : _todo });
+                                    this.props.onPinOut(item_id);
+                                }.bind(this)}>Unpin</div>
                             </li>
                             <li className={!this.state.todo.memo ? 'item-tooltip-li' : 'item-tooltip-li memo-unable'}>
                                 <div className="item-memo-icon"></div>
@@ -41,8 +71,10 @@ class ListItem extends Component {
                                     document.getElementById(`item-memo-input-${item_id}`).classList.toggle('item-memo-input-open');
                                     document.getElementById(`item-memo-input-${item_id}`).classList.toggle('item-memo-input-close');
 
-                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
-                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');
+                                    /*document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
+                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');*/
+
+                                    document.getElementById(`item-memo-input-${item_id}`).focus();
                                 }}>Add a memo</div>
                             </li>
                             <li className={this.state.todo.memo ? 'item-tooltip-li' : 'item-tooltip-li memo-unable'} id={`item-memo-delete-${item_id}`}>
@@ -50,8 +82,8 @@ class ListItem extends Component {
                                 <div className="item-memo-delete-text" onClick={function() {
                                     const _todo = Object.assign({}, this.state.todo);
 
-                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
-                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');
+                                    /*document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-open');
+                                    document.getElementById(`item-tooltip-${item_id}`).classList.toggle('item-tooltip-close');*/
 
                                     _todo.memo = "";
                                     this.setState({ todo : _todo });
@@ -83,7 +115,7 @@ class ListItem extends Component {
                     <input type="text" name="memo_input" className="item-memo-input item-memo-input-close" id={`item-memo-input-${item_id}`} placeholder="Add a memo..." autoComplete="off"></input>
 
                 </form>
-                <div className={!this.state.todo.memo ? "item-memo item-memo-close" : "item-memo item-memo-open"} id={`item-memo-${item_id}`}>{this.state.todo.memo}</div>
+                <div className={!this.state.todo.memo ? "item-memo item-memo-close" : (this.props.last_id === item_id ? "item-memo item-memo-open pin-bottom-memo" : "item-memo item-memo-open")} id={`item-memo-${item_id}`}>{this.state.todo.memo}</div>
             </div>
         );
     }
